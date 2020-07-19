509f904b30ea73f59a90d1f6c5b5f7651671703c
Samuel Schnurr <ssn@objektkultur.de>
2020-07-19 02:19:44 +0200

    Kamera Handling ist verbessert

--- Assets/Scripts/Players/PlayerCameraController.cs
+++ Assets/Scripts/Players/PlayerCameraController.cs
@@ -4,41 +4,29 @@ using UnityEngine;
 
 public class PlayerCameraController : MonoBehaviour
 {
+    public float RotationSpeed = 100f;
     private const float Y_ANGLE_MIN = -35f;
     private const float Y_ANGLE_MAX = 60f;
-
-    public float RotationSpeed = 1f;    
-    private Transform target, player;    
-    private float mouseX, mouseY;        
+    private GameObject player;
+    private float mouseX, mouseY;
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player").transform;
-        target = GameObject.FindWithTag("Target").transform;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        player = GameObject.FindWithTag("Player");
     }
     void Update()
     {
         mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
         mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
         mouseY = Mathf.Clamp(mouseY, Y_ANGLE_MIN, Y_ANGLE_MAX);
-    }
-
-    void LateUpdate()
-    {
-        ControlCamera();
-    }
-
-    
-    private void ControlCamera()
-    {
-        transform.LookAt(target);
-        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-
-        if (!Input.GetKey(KeyCode.LeftShift))
-        {
-            player.rotation = Quaternion.Euler(0, mouseX, 0);
+        
+        transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.transform.rotation = Quaternion.Euler(0, mouseX, 0);
         }
+
+
+        
     }
 }
