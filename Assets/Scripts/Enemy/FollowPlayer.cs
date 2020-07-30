2b3f89f078bbe51ee8528216b4dba38b7c0ebd04
Samuel Schnurr <ssn@objektkultur.de>
2020-07-30 21:14:59 +0200

    FollowPlayer Skript ist aufgeräumt

--- Assets/Scripts/Enemy/FollowPlayer.cs
+++ Assets/Scripts/Enemy/FollowPlayer.cs
@@ -3,28 +3,30 @@ using UnityEngine.AI;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public float MaxRadius = 15f;
     public float MaxAngle = 45f;
-    public float MaxDetection = 15f;
+    public float MaxDetectionRadius = 15f;    
+    public float MaxFollowRadius = 45f;
     private GameObject player;
     private NavMeshAgent follower;
-    private bool playerDetected;
+    private bool targetIsInFieldOfView;
+    private bool targetIsInReminder;
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, MaxRadius);
-        Vector3 line1 = Quaternion.AngleAxis(MaxAngle, transform.up) * transform.forward * MaxRadius;
-        Vector3 line2 = Quaternion.AngleAxis(-MaxAngle, transform.up) * transform.forward * MaxRadius;
+        Gizmos.DrawWireSphere(transform.position, MaxDetectionRadius);
+        Gizmos.DrawWireSphere(transform.position, MaxFollowRadius);
 
         Gizmos.color = Color.blue;
-        Gizmos.DrawRay(transform.position, line1);
-        Gizmos.DrawRay(transform.position, line2);
+        Vector3 leftAngle = Quaternion.AngleAxis(-MaxAngle, transform.up) * transform.forward * MaxDetectionRadius;
+        Vector3 rightAngle = Quaternion.AngleAxis(MaxAngle, transform.up) * transform.forward * MaxDetectionRadius;
+        Gizmos.DrawRay(transform.position, leftAngle);
+        Gizmos.DrawRay(transform.position, rightAngle);
 
         Gizmos.color = Color.black;
-        Gizmos.DrawRay(transform.position, transform.forward * MaxRadius);
+        Gizmos.DrawRay(transform.position, transform.forward * MaxDetectionRadius);
 
-        if (!playerDetected)
+        if (targetIsInFieldOfView)
         {
             Gizmos.color = Color.red;
         }
@@ -33,83 +35,84 @@ public class FollowPlayer : MonoBehaviour
             Gizmos.color = Color.green;
         }
 
-        Gizmos.DrawRay(transform.position, (player.transform.position - transform.position).normalized * MaxRadius);
+        Gizmos.DrawRay(transform.position, (player.transform.position - transform.position).normalized * MaxDetectionRadius);
     }
 
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         follower = GetComponent<NavMeshAgent>();
-    }    
-
-    public static bool Test(Transform checkingObject, Transform target, float maxAngle, float maxRadius)
-    {
-        Collider[] overlaps = Physics.OverlapSphere(checkingObject.position, maxRadius);
-
-        foreach (Collider overlap in overlaps)
-        {
-            if (overlap.transform == target)
-            {
-                // Normalize when you only care about the direction but not about the distance
-                Vector3 directionBetween = (target.position - checkingObject.position).normalized;
-                
-                // The height is not a factor in the angle
-                directionBetween.y *= 0;
-
-                float angle = Vector3.Angle(checkingObject.forward, directionBetween);
-
-                if(angle <= maxAngle)
-                {
-                    Ray ray = new Ray(checkingObject.position, target.position - checkingObject.position);
-                    RaycastHit hit;
-
-                    if(Physics.Raycast(ray, out hit, maxRadius))
-                    {
-                        if(hit.transform == target)
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
     }
 
     void Update()
     {
-        playerDetected = Test(transform, player.transform, MaxAngle, MaxRadius);
-        
-        //float distance = Vector3.Distance(transform.position, player.transform.position);
+        targetIsInFieldOfView = IsInFieldOfView(transform, player.transform, MaxAngle, MaxDetectionRadius);
 
-        //// Player is in distance
-        //if(distance < MaxRadius)
-        //{
-        //    RaycastHit hit;
+        if (targetIsInFieldOfView)
+        {
+            targetIsInReminder = true;
+        }
 
-        //    // Check if a collider is hit
-        //    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, MaxDetection))
-        //    {
-        //        // Check if player is hit
-        //        if (hit.collider.gameObject.CompareTag("Player"))
-        //        {
-        //            playerDetected = true;
-        //        }
-        //    }
+        if (targetIsInReminder)
+        {
+            targetIsInReminder = FollowTarget(transform, player.transform, follower, MaxFollowRadius);
+        }
+    }
 
-        //    if (playerDetected)
-        //    {
-        //        // Follow the player
-        //        Vector3 directionToPlayer = transform.position - player.transform.position;
-        //        Vector3 newPosition = transform.position - directionToPlayer;
-        //        follower.SetDestination(newPosition);
-        //    }
-        //}
-        //else
-        //{
-        //    // Max Distance is reached
-        //    // Focus to player is lost
-        //    playerDetected = false;
-        //}
+    private static bool IsInFieldOfView(Transform origin, Transform target, float maxAngle, float maxRadius)
+    {
+        // Get all colliders within the radius ob the origin object
+        Collider[] overlaps = Physics.OverlapSphere(origin.position, maxRadius);
+
+        foreach (Collider overlap in overlaps)
+        {
+            if (overlap.transform != target)
+            {
+                continue;
+            }
+
+            // Normalize when you only care about the direction but not about the distance
+            Vector3 directionToTarget = (target.position - origin.position).normalized;
+
+            // The height is not a factor in the field of view angle
+            directionToTarget.y *= 0;
+
+            // The angle the origin objects needs, to see the target
+            float angle = Vector3.Angle(origin.forward, directionToTarget);
+
+            // The angle is within the field of view of the origin object
+            if (angle <= maxAngle)
+            {
+                Vector3 distanceToTarget = target.position - origin.position;
+                Ray ray = new Ray(origin.position, distanceToTarget);
+
+                // Raycast to check if there are obstacles in front of the target
+                if (Physics.Raycast(ray, out RaycastHit hit, maxRadius))
+                {
+                    if (hit.transform == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static bool FollowTarget(Transform origin, Transform target, NavMeshAgent navMeshAgent, float maxRadius)
+    {
+        float distance = Vector3.Distance(origin.position, target.position);
+
+        // Target is in distance
+        if (distance <= maxRadius)
+        {
+            Vector3 directionToTarget = origin.position - target.position;
+            Vector3 newPosition = origin.position - directionToTarget;
+            navMeshAgent.SetDestination(newPosition);
+            return true;
+        }
+
+        return false;
     }
 }
