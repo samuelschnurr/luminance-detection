4127c311cad146c614ac745ce7422cac23ff0eae
Samuel Schnurr <samuelschnurr@live.de>
2020-09-19 13:33:30 +0200

    Created PlayerInput Handler

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    /// <summary>
+    /// Basi class for player input
+    /// </summary>
+    public class PlayerInput : MonoBehaviour
+    {
+        public float MoveX { get; private set; }
+        public float MoveZ { get; private set; }
+        public float MouseX { get; private set; }
+        public float MouseY { get; private set; }
+        public bool Jump { get; private set; }
+
+        // Use Update() to get the user input per each frame
+        // Player input will not be get lost on slow/fast frame rates
+        void Update()
+        {
+            MoveX = Input.GetAxis("Horizontal");
+            MoveZ = Input.GetAxis("Vertical");
+            MouseX = Input.GetAxis("Mouse X");
+            MouseY = Input.GetAxis("Mouse Y");
+            Jump = Input.GetButtonDown("Jump");            
+        }
+    }
+}
