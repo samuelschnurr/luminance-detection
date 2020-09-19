4d1d1db757538019cbe445fe3733323404573ce7
Samuel Schnurr <samuelschnurr@live.de>
2020-09-19 13:34:25 +0200

    Changed PlayerCamera to InputHandler changed Movement to LateUpdate and added RequiredComponent

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -5,12 +5,14 @@ namespace Assets.Scripts.Player
     /// <summary>
     /// Player camera behaviour
     /// </summary>
+    [RequireComponent(typeof(PlayerInput))]
     public class PlayerCamera : MonoBehaviour
     {
-        public float RotationSpeed = 100f;
+        public float RotationSpeed = 1f;
         private const float Y_ANGLE_MIN = -35f;
         private const float Y_ANGLE_MAX = 60f;
         private GameObject player;
+        private PlayerInput playerInput;
         private float mouseX, mouseY;
 
         void Start()
@@ -18,13 +20,29 @@ namespace Assets.Scripts.Player
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
             player = GameObject.FindWithTag("Player");
+            playerInput = GetComponent<PlayerInput>();
         }
 
-        void FixedUpdate()
+        void Update()
         {
-            mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
-            mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+            CalculateCameraMovement();
+        }
+
+        // Execute camera movement after all physics happened
+        void LateUpdate()
+        {
+            MoveCamera();
+        }
+
+        private void CalculateCameraMovement()
+        {
+            mouseX += playerInput.MouseX * RotationSpeed;
+            mouseY -= playerInput.MouseY * RotationSpeed;
             mouseY = Mathf.Clamp(mouseY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        }
+
+        private void MoveCamera()
+        {
             transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);
             player.transform.rotation = Quaternion.Euler(0, mouseX, 0);
         }
